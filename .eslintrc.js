3badb3d3d75bf2082e67bbef756dd4e74d0a3dc1
lnk <wyy102489@outlook.com>
2018-10-25 18:15:40 +0800

    feat: add test case

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     extends: 'airbnb-base',
     env: {
         'es6': true,
+        'mocha': true,
     },
     parserOptions: {
         'ecmaVersion': 6,
--- test/main.js
+++ test/main.js
@@ -1,80 +1,117 @@
-import {assert} from 'chai';
+import {assert} from 'chai'
 import LemoClient from '../lib/main'
 import HttpConn from '../lib/conn/http_conn'
+import {resetRPC} from '../lib/jsonrpc'
 
 function createTestConn() {
     let sendRecord = null
     const conn = {
         send(...args) {
-            sendRecord = JSON.stringify(args)
+            sendRecord = args
             return {jsonrpc: '2.0', id: 1, result: {}}
         }
     }
     return [conn, () => sendRecord]
 }
 
-describe('LemoClient_new', function() {
-    it('default conn', function() {
+describe('LemoClient_new', () => {
+    beforeEach(() => {
+        resetRPC()
+    })
+
+    it('default conn', () => {
         const lemo = new LemoClient()
-        assert.equal(lemo._requester.conn instanceof HttpConn, true);
-    });
-    it('http conn', function() {
+        assert.equal(lemo._requester.conn instanceof HttpConn, true)
+    })
+    it('http conn', () => {
         const lemo = new LemoClient({host: 'http://127.0.0.1:8002'})
-        assert.equal(lemo._requester.conn instanceof HttpConn, true);
-        assert.equal(lemo._requester.conn.host, 'http://127.0.0.1:8002');
-    });
-    it('http conn localhost', function() {
+        assert.equal(lemo._requester.conn instanceof HttpConn, true)
+        assert.equal(lemo._requester.conn.host, 'http://127.0.0.1:8002')
+    })
+    it('http conn localhost', () => {
         const lemo = new LemoClient({host: 'http://localhost:8002'})
-        assert.equal(lemo._requester.conn.host, 'http://localhost:8002');
-    });
-    it('unknown conn', function() {
+        assert.equal(lemo._requester.conn.host, 'http://localhost:8002')
+    })
+    it('unknown conn', () => {
         const config = {host: 'abc'}
         assert.throws(() => {
             new LemoClient(config)
-        }, `unknown conn config: ${config}`);
-    });
-    it('custom conn', function() {
+        }, `unknown conn config: ${config}`)
+    })
+    it('custom conn', async () => {
         const [testConn, getRecord] = createTestConn()
         const lemo = new LemoClient(testConn)
-        assert.equal(lemo._requester.conn, testConn);
-        lemo.getCurrentBlock()
-        assert.equal(getRecord(), JSON.stringify([{
-            "jsonrpc": "2.0",
-            "id": 1,
-            "method": "chain_latestStableBlock",
-            "params": [null]
-        }]));
-    });
-});
-
-describe('LemoClient__createAPI', function() {
-    it('aaa.bbb', function() {
-        const [testConn, getRecord] = createTestConn()
-        const lemo = new LemoClient(testConn)
-        lemo._createAPI('aaa', [{
-            name: 'bbb',
-            method: 'ccc',
+        assert.equal(lemo._requester.conn, testConn)
+        await lemo.getCurrentBlock()
+        assert.deepEqual(getRecord(), [{
+            'jsonrpc': '2.0',
+            'id': 1,
+            'method': 'chain_latestStableBlock',
+            'params': [undefined]
         }])
-        lemo.aaa.bbb(123, '8293')
-        assert.equal(getRecord(), JSON.stringify([{
-            "jsonrpc": "2.0",
-            "id": 1,
-            "method": "ccc",
-            "params": [123, '8293']
-        }]));
-    });
-});
+    })
+})
 
+describe('LemoClient__createAPI', () => {
+    beforeEach(() => {
+        resetRPC()
+    })
 
-async function test() {
-    const lemo = new LemoClient()
-    lemo.getCurrentBlock(true)
-        .then(d => console.log(d))
-    lemo.getNodeVersion()
-        .then(d => console.log(d))
+    it('lemo.test.setData', async () => {
+        const [testConn, getRecord] = createTestConn()
+        const lemo = new LemoClient(testConn)
+        lemo._createAPI('test', [{
+            name: 'setData',
+            method: 'api_name',
+        }])
+        await lemo.test.setData(123, {a: '8293'})
+        assert.deepEqual(getRecord(), [{
+            'jsonrpc': '2.0',
+            'id': 1,
+            'method': 'api_name',
+            'params': [123, {a: '8293'}]
+        }])
+    })
 
-    const watchId = lemo.watchBlock(false, newBlock => console.log(newBlock))
-    setTimeout(() => {
-        lemo.stopWatch(watchId)
-    }, 10000)
-}
+    it('no module name', async () => {
+        const [testConn, getRecord] = createTestConn()
+        const lemo = new LemoClient(testConn)
+        lemo._createAPI('', [{
+            name: 'setData',
+            method: 'api_name',
+        }])
+        await lemo.setData(123)
+        assert.deepEqual(getRecord(), [{
+            'jsonrpc': '2.0',
+            'id': 1,
+            'method': 'api_name',
+            'params': [123]
+        }])
+    })
+
+    it('custom call', async () => {
+        const [testConn] = createTestConn()
+        const lemo = new LemoClient(testConn)
+        lemo._createAPI('', [{
+            name: 'callMyFunc',
+            call: (requester, ...args) => {
+                assert.equal(requester, lemo._requester)
+                assert.deepEqual(args, [123, '8293'])
+                return 100
+            },
+        }])
+        const result = lemo.callMyFunc(123, '8293')
+        assert.equal(result, 100)
+    })
+
+    it('custom async call', async () => {
+        const [testConn] = createTestConn()
+        const lemo = new LemoClient(testConn)
+        lemo._createAPI('', [{
+            name: 'callMyFunc',
+            call: () => Promise.resolve(200),
+        }])
+        const result = await lemo.callMyFunc()
+        assert.equal(result, 200)
+    })
+})
