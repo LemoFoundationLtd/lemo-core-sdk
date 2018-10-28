6d57c13f92eb20f84369c6e8f00d8a1624f88a5d
lnk <wyy102489@outlook.com>
2018-10-29 02:03:43 +0800

    feat: finish tx api

--- test/modules/account.js
+++ test/modules/account.js
@@ -1,50 +1,10 @@
 import {assert} from 'chai'
-import nock from 'nock'
 import BigNumber from 'bignumber.js'
-import {DEFAULT_HTTP_HOST} from '../../lib/config'
 import LemoClient from '../../lib/main'
-
-const lemoBase = {
-    'address': '0x015780f8456f9c1532645087a19dcf9a7e0c7f97',
-    'balance': '0x52b7d2dcc80cd2e40000000',
-    'codeHash': '0x0000000000000000000000000000000000000000000000000000000000000000',
-    'records': {
-        '1': {
-            'Height': 1,
-            'Version': 1,
-        },
-    },
-    'root': '0x0000000000000000000000000000000000000000000000000000000000000000',
-}
-const emptyAccount = {
-    'balance': '0x0',
-    'codeHash': '0x0000000000000000000000000000000000000000000000000000000000000000',
-    'records': {},
-    'root': '0x0000000000000000000000000000000000000000000000000000000000000000',
-}
+import {lemoBase} from '../datas'
+import '../mock'
 
 describe('account_getAccount', () => {
-    beforeEach(() => {
-        nock(DEFAULT_HTTP_HOST)
-            // .log(console.log)
-            .post('/', {jsonrpc: '2.0', id: /\d+/, method: 'account_getAccount', params: [/.*/]})
-            .reply((uri, requestBody) => {
-                const result = (requestBody.params[0] === lemoBase.address) ? lemoBase : emptyAccount
-                result.address = requestBody.params[0]
-                return [200, {
-                    jsonrpc: '2.0',
-                    id: 123,
-                    result,
-                }]
-            })
-        // .post('/', {jsonrpc: '2.0', id: /\d+/, method: 'account_getBalance', params: [/.*/]})
-        // .reply(200, {
-        //     jsonrpc: '2.0',R
-        //     id: 123,
-        //     result: '10000001000000000000000000',
-        // })
-    })
-
     it('account with balance', async () => {
         const lemo = new LemoClient()
         const result = await lemo.account.getAccount(lemoBase.address)
--- test/modules/tx.js
+++ test/modules/tx.js
@@ -1,46 +1,25 @@
 import {assert} from 'chai'
-import nock from 'nock'
-import BigNumber from 'bignumber.js'
-import {DEFAULT_HTTP_HOST} from '../../lib/config'
 import LemoClient from '../../lib/main'
-import Tx from '../../lib/tx'
-
+import {testTxs, testPrivate} from '../datas'
+import '../mock'
 
 describe('module_tx_sendTx', () => {
-    beforeEach(() => {
-        // nock(DEFAULT_HTTP_HOST)
-        //     // .log(console.log)
-        //     .post('/', {jsonrpc: '2.0', id: /\d+/, method: 'account_getAccount', params: [/.*/]})
-        //     .reply((uri, requestBody) => {
-        //         const result = (requestBody.params[0] === lemoBase.address) ? lemoBase : emptyAccount
-        //         result.address = requestBody.params[0]
-        //         return [200, {
-        //             jsonrpc: '2.0',
-        //             id: 123,
-        //             result,
-        //         }]
-        //     })
-        // .post('/', {jsonrpc: '2.0', id: /\d+/, method: 'account_getBalance', params: [/.*/]})
-        // .reply(200, {
-        //     jsonrpc: '2.0',R
-        //     id: 123,
-        //     result: '10000001000000000000000000',
-        // })
+    it('sendTx', () => {
+        testTxs.forEach(async (test, i) => {
+            const lemo = new LemoClient()
+            const result = await lemo.tx.sendTx(testPrivate, test.txConfig)
+            assert.equal(result, test.hashAfterSign, `index=${i}`)
+        })
     })
+})
 
-    it('sign', async () => {
-        const lemo = new LemoClient()
-        const result = lemo.tx.sign({
-            to: '0x1',
-            amount: 1,
-            gasLimit: 100,
-            gasPrice: 2,
-            data: 12,
-            chainId: 200,
-            expirationTime: 1544584596,
-            toName: 'aa',
-            message: 34,
-        }, '0x432a86ab8765d82415a803e29864dcfc1ed93dac949abf6f95a583179f27e4bb')
-        assert.equal(result, '0xf8689400000000000000000000000000000000000000018261610264010c845c107d9422830300c8a0158b80d695e7d543ddb3ae09ed89b0fdd0c9f72b95a96e5f2b5e67a4d6d71a88a02b893b663e36f997df1e3f489b98d001cf615ee1e32b3c28ce6364f5cc681d5c')
+describe('module_tx_sign_send', () => {
+    it('sign', () => {
+        testTxs.forEach(async (test, i) => {
+            const lemo = new LemoClient()
+            const json = await lemo.tx.sign(testPrivate, test.txConfig)
+            const result = await lemo.tx.send(JSON.parse(json))
+            assert.equal(result, test.hashAfterSign, `index=${i}`)
+        })
     })
 })
