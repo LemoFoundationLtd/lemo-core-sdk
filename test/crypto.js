6bc2ecd8d402d0e503daa74453ca9bed3d00f921
lnk <wyy102489@outlook.com>
2018-11-16 17:05:21 +0800

    fix: fix crypto test case, add more comments

--- test/crypto.js
+++ test/crypto.js
@@ -4,15 +4,16 @@ import {toBuffer} from '../lib/utils'
 import {testTxs} from './datas'
 
 const testPrivate = '0xc21b6b2fbf230f665b936194d14da67187732bf9d28768aef1a3cbb26608f8aa'
-const pubKey = '0x5e3600755f9b512a65603b38e30885c98cbac70259c3235c9b3f42ee563b480edea351ba0ff5748a638fe0aeff5d845bf37a3b437831871b48fd32f33cd9a3c0'
+const pubKey = '0x045e3600755f9b512a65603b38e30885c98cbac70259c3235c9b3f42ee563b480edea351ba0ff5748a638fe0aeff5d845bf37a3b437831871b48fd32f33cd9a3c0'
 const pubAddress = 'Lemo3J2JD6T9GPB9F2GSG5KBP9PF5S3RDPKJ74C'
 
 describe('crypto_recover', () => {
     it('recover', () => {
         testTxs.forEach(test => {
             const signdResult = sign(toBuffer(testPrivate), toBuffer(test.hash))
-            const result = recover(test.hash, signdResult.recover, signdResult.r, signdResult.s)
-            assert.strictEqual(result, null)
+            const result = recover(toBuffer(test.hash), signdResult.recovery, signdResult.r, signdResult.s)
+            assert.exists(result)
+            assert.strictEqual(`0x${result.toString('hex')}`, pubKey)
         })
     })
 })
