8f12148aa189b40852fe104f13f0f0658ceae6d0
mrxiao6 <1306229903@qq.com>
2019-03-15 18:16:10 +0800

    fix

--- test/watchers/blocks_watcher.js
+++ test/watchers/blocks_watcher.js
@@ -1,9 +1,10 @@
 import {assert} from 'chai'
 import Watcher from '../../lib/watchers/blocks_watcher'
 
-describe('network_blocks_processor', () => {
+const watcher = new Watcher()
+describe('watchers_processor', () => {
     it('processBlock', () => {
-        Watcher.clearHistory()
+        watcher.clearHistory()
         const testArr = [1, 3, 4, 6, 8].map((item) => {
             return {
                 header: {
@@ -20,7 +21,7 @@ describe('network_blocks_processor', () => {
         }
         let lastBlockHeight = 0
         testArr.forEach((item) => {
-            Watcher.processBlock(testFetch, item, (block) => {
+            watcher.processBlock(testFetch, item, (block) => {
                 lastBlockHeight++
                 assert.deepEqual(lastBlockHeight, block.header.height)
             })
