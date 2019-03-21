488edd4c6e91b0a87de8f676869b4b6952f566a4
mrxiao6 <1306229903@qq.com>
2019-03-21 19:03:00 +0800

    fix

--- test/watchers/tx_watcher.js
+++ test/watchers/tx_watcher.js
@@ -4,33 +4,24 @@ import {DEFAULT_POLL_DURATION} from '../../lib/config'
 import Requester from '../../lib/network/requester'
 
 describe('Requester_tx_watcher', () => {
-    it('tx_watcher',  async function itFunc(done) {
+    it('tx_watcher',   function itFunc(done) {
         this.timeout(DEFAULT_POLL_DURATION + 1000)
-        const responses = new Array(3).fill({jsonrpc: '2.0', id: 1, result: 'hash'})
-        const hash = '0xfc4e1eccdc7e199336503ae67da0ee66eb46e1f953f65f22c8b62b53db76a103'
-        let times = 0
+        const responses = [{jsonrpc: '2.0', id: 1, result: null}, {jsonrpc: '2.0', id: 2, result: 'hash'}]
+        const testTxHash = '0xfc4e1eccdc7e199336503ae67da0ee66eb46e1f953f65f22c8b62b53db76a103'
+        const error = new Error('waitTx too many times')
         const conn = {
             async send() {
-                if (responses.length === 1) {
-                    assert.equal(times, 2)
-                    requester.stopWatch()
-                    done()
-                    return responses.pop()
+                if (!responses.length) {
+                    throw error
                 }
-                times++
-                return responses.pop()
+                return responses.shift()
             },
         }
         const requester = new Requester(conn, {maxPollRetry: 0})
         const txWatcher = new TxWatcher(requester)
-        txWatcher.pollTxHash(hash).then(txhash => {
-            assert.equal(txhash, hash)
-        })
-        txWatcher.pollTxHash(hash).then(txhash => {
-            assert.equal(txhash, hash)
-        })
-        txWatcher.pollTxHash(hash).then(txhash => {
-            assert.equal(txhash, hash)
+        txWatcher.waitTx(testTxHash).then(txHash => {
+            assert.equal(txHash, testTxHash)
+            done()
         })
     })
 })
