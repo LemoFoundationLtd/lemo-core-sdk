b0edcb624a6570d6d9241d8fc6256803aff8121c
mrxiao6 <1306229903@qq.com>
2019-03-22 18:01:26 +0800

    fix

--- test/watchers/tx_watcher.js
+++ test/watchers/tx_watcher.js
@@ -1,7 +1,11 @@
 import {assert} from 'chai'
 import TxWatcher from '../../lib/watchers/tx_watcher'
+import BlockWatcher from '../../lib/watchers/block_watcher'
 import {DEFAULT_POLL_DURATION} from '../../lib/config'
 import Requester from '../../lib/network/requester'
+import HttpConn from '../../lib/network/conn/http_conn'
+import errors from '../../lib/errors'
+import '../mock'
 
 describe('module_tx_watcher', () => {
     it('tx_watcher', function itFunc(done) {
@@ -22,6 +26,23 @@ describe('module_tx_watcher', () => {
         txWatcher.waitTx(testTxHash).then(txHash => {
             assert.equal(txHash, testTxHash)
             done()
+        })
+    })
+    it('server_mode_has_hash', async () => {
+        const requester = new Requester(new HttpConn('http://127.0.0.1:8001'), {maxPollRetry: 0})
+        const blockWatcher = new BlockWatcher(requester)
+        const txWatcher = new TxWatcher(requester, blockWatcher, true)
+        const hash = '0x314f1b9c8585e53446983e68fdbf6642e00e5b58cfde9165fdec051cfb21d157'
+        const txHash = await txWatcher.waitTx(hash)
+        return  assert.equal(hash, txHash)
+    })
+    it('server_mode_timeOut', async () => {
+        const requester = new Requester(new HttpConn('http://127.0.0.1:8001'), {maxPollRetry: 0})
+        const blockWatcher = new BlockWatcher(requester)
+        const txWatcher = new TxWatcher(requester, blockWatcher, true)
+        const hash = ''
+        txWatcher.waitTx(hash).catch(e => {
+            return assert.equal(e, errors.InvalidPollTxTimeOut())
         })
     })
 })
