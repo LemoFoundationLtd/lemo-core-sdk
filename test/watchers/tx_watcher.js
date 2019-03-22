742cdbbb6d5a27b90425f4cd81ff990e3cede0ef
mrxiao6 <1306229903@qq.com>
2019-03-22 14:06:45 +0800

    fix

--- test/watchers/tx_watcher.js
+++ test/watchers/tx_watcher.js
@@ -3,7 +3,7 @@ import TxWatcher from '../../lib/watchers/tx_watcher'
 import {DEFAULT_POLL_DURATION} from '../../lib/config'
 import Requester from '../../lib/network/requester'
 
-describe('Requester_tx_watcher', () => {
+describe('module_tx_watcher', () => {
     it('tx_watcher',   function itFunc(done) {
         this.timeout(DEFAULT_POLL_DURATION + 1000)
         const responses = [{jsonrpc: '2.0', id: 1, result: null}, {jsonrpc: '2.0', id: 2, result: 'hash'}]
