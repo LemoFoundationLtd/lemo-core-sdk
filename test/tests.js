78e311e7e9c6db06c7b11702bbcb5219d993eaf2
mrxiao6 <1306229903@qq.com>
2019-03-30 16:01:57 +0800

    feat: browser

---
+++ test/tests.js
@@ -0,0 +1,42 @@
+import BigNumber from 'bignumber.js'
+import utils from '../lib/utils'
+
+const assert = chai.should()
+
+describe('formatMoney', () => {
+    const tests = {
+        0: '0 LEMO',
+        1: '1 mo',
+        100: '100 mo',
+        1000: '1K mo',
+        1001: '1001 mo',
+        10010000: '10010K mo',
+        10000000: '10M mo',
+        1000000000: '1G mo',
+        100000000000: '100G mo',
+        1000000000000: '0.000001 LEMO',
+        1000000000001: '0.000001000000000001 LEMO',
+        11000000000000: '0.000011 LEMO',
+        11111000000000000: '0.011111 LEMO',
+        11110001000000000000: '11.110001 LEMO',
+        11110001001000000000: '11.110001001 LEMO',
+        111110001000000000000: '111.110001 LEMO',
+        1000000000000000000: '1 LEMO',
+        100000000000000000000: '100 LEMO',
+    }
+    Object.entries(tests).forEach(([input, output]) => {
+        it(`number ${input}`, () => {
+            assert.equal(utils.formatMoney(input), output)
+        })
+    })
+    Object.entries(tests).forEach(([input, output]) => {
+        it(`string ${input}`, () => {
+            assert.equal(utils.formatMoney(`${input}`), output)
+        })
+    })
+    Object.entries(tests).forEach(([input, output]) => {
+        it(`BigNumber ${input}`, () => {
+            assert.equal(utils.formatMoney(new BigNumber(input)), output)
+        })
+    })
+})
