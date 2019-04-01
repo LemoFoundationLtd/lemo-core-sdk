4288f9f94c8c1addbb4e15c71a09dc3a00385355
mrxiao6 <1306229903@qq.com>
2019-04-01 11:07:20 +0800

    fix

--- test/browser/tx.js
+++ test/browser/tx.js
@@ -1,30 +1,20 @@
-
 (() => {
     const testPrivate = '0x432a86ab8765d82415a803e29864dcfc1ed93dac949abf6f95a583179f27e4bb'
     const assert = chai.should()
     const chainID = 200
-    const txInfo = {
+    const emptyTxInfo = {
         txConfig: {
             chainID,
-            version: 1,
-            type: 0,
-            to: '0x000000000000000000000001',
-            toName: 'aa',
-            gasPrice: 2,
-            gasLimit: 100,
-            amount: 1,
-            data: '0x0c',
             expirationTime: 1544584596,
-            message: 'aaa',
         },
-        rlp: '0xea800181c89400000000000000000000000000000000000000018261610264010c845c107d948361616180',
-        hash: '0x07bbd852209e19dc477b37fcf54d77c1cccfc65af67cd1b9203d18bc19f08024',
+        rlp: '0xd8800181c8808084b2d05e00831e84808080845c107d948080',
+        hash: '0x13dd44355f872cda26cf08d42084793f9b6b6c2129e8d70a73c1c8fa62cf7f05',
         rlpAfterSign:
-        '0xf86c800181c89400000000000000000000000000000000000000018261610264010c845c107d9483616161b8418c0499083cb3d27bead4f21994aeebf8e75fa11df6bfe01c71cad583fc9a3c70778a437607d072540719a866adb630001fabbfb6b032d1a8dfbffac7daed8f0201',
-        hashAfterSign: '0xe50395127cdf65b131eb39de393deb2d61f8f6645e4bee3496ab4a743b520496',
+        '0xf85a800181c8808084b2d05e00831e84808080845c107d9480b841f642fbc4588fbab945a6db57381fb756221607c96f5519c5f5092ca212b454e7529b1c78da1927bc99d07f0b0f3e18442b6d911ce71a45a6f0da101e84b88e3c01',
+        hashAfterSign: '0x37e2548baa166815431afc470fc65dfc5a399959fb179d3d33ebc8d561697ae6',
     }
 
-    const responses = {jsonrpc: '2.0', id: 1, result: txInfo.hashAfterSign}
+    const responses = {jsonrpc: '2.0', id: 1, result: emptyTxInfo.hashAfterSign}
     const conn = {
         chainID,
         async send() {
@@ -32,16 +22,12 @@
         },
     }
 
-    describe('module_tx_sendTx', () => {
-        it('sendTx_with_hex_address_without_waitConfirm', async () => {
+    describe('module_tx_sign_send', () => {
+        it('sign_send_with_hex_address', async () => {
             const lemo = new LemoClient(conn)
-            const result = await lemo.tx.sendTx(testPrivate, txInfo.txConfig, false)
-            return assert.equal(result,  txInfo.hashAfterSign)
-        })
-        it('sendTx_with_hex_address_waitConfirm', async () => {
-            const lemo = new LemoClient(conn)
-            const result = await lemo.tx.sendTx(testPrivate, txInfo.txConfig, true)
-            return assert.equal(result,  txInfo.hashAfterSign)
+            const json = await lemo.tx.sign(testPrivate, emptyTxInfo.txConfig)
+            const result = await lemo.tx.send(json)
+            assert.equal(result, emptyTxInfo.hashAfterSign)
         })
     })
 })()
