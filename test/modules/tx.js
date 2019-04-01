d5dcbc0b65bed08337811d07055c96a5e10a5395
yAmanda <amandayang@inslabs.org>
2019-04-01 18:19:44 +0800

    fix(tx_sendTx): remove test about sendTx timeout

--- test/modules/tx.js
+++ test/modules/tx.js
@@ -63,12 +63,6 @@ describe('module_tx_sendTx', () => {
             }),
         )
     })
-    it('sendTx_with_hex_address_timeOut', () => {
-        const lemo = new LemoClient({chainID})
-        lemo.tx.sendTx(testPrivate, tx4, true).catch(e => {
-            return assert.equal(e, errors.InvalidPollTxTimeOut())
-        })
-    })
     it('sendTx_with_lemo_address_without_waitConfirm', async () => {
         const lemo = new LemoClient({chainID})
         const result = await lemo.tx.sendTx(testPrivate, bigTxInfoWithLemoAddr.txConfig, false)
