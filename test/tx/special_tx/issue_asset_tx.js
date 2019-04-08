1c582c073051dd0b07c80248064fe51e20e41243
mrxiao6 <1306229903@qq.com>
2019-04-08 18:36:51 +0800

    fix

--- test/tx/special_tx/issue_asset_tx.js
+++ test/tx/special_tx/issue_asset_tx.js
@@ -35,6 +35,14 @@ describe('IssueAsset_new', () => {
             new IssueAsset({chainID, to: 'lemobw', toName: 'alice'}, issueAssetInfo)
         }, errors.TXParamMissingError('assetCode'))
     })
+    it('miss config.supplyAmount', () => {
+        const issueAssetInfo = {
+            assetCode: '0xd0befd3850c574b7f6ad6f7943fe19b212affb90162978adc2193a035ced8884',
+        }
+        assert.throws(() => {
+            new IssueAsset({chainID, to: 'lemobw', toName: 'alice'}, issueAssetInfo)
+        }, errors.TXParamMissingError('supplyAmount'))
+    })
     it('normal config', () => {
         const issueAssetInfo = {
             assetCode: '0xd0befd3850c574b7f6ad6f7943fe19b212affb90162978adc2193a035ced8884',
