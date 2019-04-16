d9926313e04a243790f0fbf72e4c8a3d17ee7a28
yAmanda <amandayang@inslabs.org>
2019-04-16 11:08:54 +0800

    fix(tx): change some value and test

--- test/tx/special_tx/modify_asset_tx.js
+++ test/tx/special_tx/modify_asset_tx.js
@@ -34,7 +34,9 @@ describe('Modify-Asset', () => {
         const modifyInfo = {
             assetCode: '0xd0befd3850c574b7f6ad6f7943fe19b212affb90162978adc2193a035ced8884',
         }
-        assert.equal(modifyInfo.info, undefined)
+        assert.throws(() => {
+            new ModifyAsset({chainID, to: 'hello'}, modifyInfo)
+        }, errors.TXInfoError())
     })
 })
 
