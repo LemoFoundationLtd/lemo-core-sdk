7cb23801b0e61e2e9fed7f0e686ce7a0fa901992
yAmanda <amandayang@inslabs.org>
2019-08-05 09:32:10 +0800

    fix: add signTx and modify the returned data format

--- test/modules/static_properties.js
+++ test/modules/static_properties.js
@@ -2,7 +2,7 @@ import {assert} from 'chai'
 import {Buffer} from 'safe-buffer'
 import BigNumber from 'bignumber.js'
 import LemoCore from '../../lib/index'
-import {chainID} from '../datas'
+import {chainID, testPrivate, bigTxInfoWithLemoAddr} from '../datas'
 import errors from '../../lib/errors'
 
 describe('module_tool_verifyAddress', () => {
@@ -108,5 +108,12 @@ describe('module_tool_toBuffer', () => {
                 assert.equal(LemoCore.toBuffer(test.input).toString('hex'), test.output)
             }
         })
+    })
+})
+describe('module_tool_signTX', () => {
+    it('signTx_normal', () => {
+        let result = LemoCore.signTx(testPrivate, bigTxInfoWithLemoAddr.txConfig)
+        result = JSON.parse(result)
+        assert.equal(result.from, bigTxInfoWithLemoAddr.txConfig.from)
     })
 })
